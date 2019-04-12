82ca8f9193c4c4a9a13d200a54b9db324bc511e1
dngur6344 <dngur6344@gmail.com>
2019-04-12 20:49:23 +0900

    navigation5

---
+++ woohyuk_checklist/ChecklistItem.swift
@@ -0,0 +1,16 @@
+//
+//  ChecklistItem.swift
+//  woohyuk_checklist
+//
+//  Created by user on 12/04/2019.
+//  Copyright © 2019 woohyuk. All rights reserved.
+//
+
+import Foundation
+class ChecklistItem{
+    var text=""
+    var checked = false
+    func toggleChecked(){
+        checked = !checked
+    }
+}
